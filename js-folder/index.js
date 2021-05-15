10d01a0a81072e7e16d41200446a0c16d9534a63
smmccolley <smmccolley@gmail.com>
2021-05-15 11:59:30 -0400

    Update index.js

--- js-folder/index.js
+++ js-folder/index.js
@@ -1,7 +1,7 @@
 $('.readMore value').click(function(){
     $("modal").show("slow");
 });
-
+// I've tried and deleted different versions of this**//
 //**var modal= (".communityModal");
 //var btn=(".readMore");
 //var span=documnent.getElementsbyClassName(".close");
