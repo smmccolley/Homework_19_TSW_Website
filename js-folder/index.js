e857f64394470acd5a5435828a8f513877e4e7c2
smmccolley <smmccolley@gmail.com>
2021-05-15 11:58:55 -0400

    TSW_Website_HW19_V3
    Hover states applied; attempting jQuery

---
+++ js-folder/index.js
@@ -0,0 +1,8 @@
+$('.readMore value').click(function(){
+    $("modal").show("slow");
+});
+
+//**var modal= (".communityModal");
+//var btn=(".readMore");
+//var span=documnent.getElementsbyClassName(".close");
+//});
