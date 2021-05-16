e99ad0187d056d102f478a90885617ab1aae1762
smmccolley <smmccolley@gmail.com>
2021-05-16 11:20:01 -0400

    TSW_Website_V4
    Small graphics changes; copying over some jQuery ideas for modals

--- js-folder/index.js
+++ js-folder/index.js
@@ -6,3 +6,22 @@ $('.readMore value').click(function(){
 //var btn=(".readMore");
 //var span=documnent.getElementsbyClassName(".close");
 //});
+
+$(".readMore").dialog({
+    modal: true;
+});
+
+var modal = $(".readMore").dialog(".modal")
+$(".readMore").dialog(".modal", true);
+
+// Getter
+var position = $( ".selector" ).dialog(".modal", "position");
+ 
+// Setter
+$( ".selector" ).dialog( "option", "position", { my: "left top", at: "left bottom", of: button } );
+
+<script>
+function myFunction() { 
+  document.getElementById("myDialog").showModal(); 
+} 
+</script>
