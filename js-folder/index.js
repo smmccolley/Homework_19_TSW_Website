69ad0f7ba68462cbe0c581ab4c44da160b7d67b0
smmccolley <smmccolley@gmail.com>
2021-05-16 19:32:01 -0400

    TSW_Website_V6
    Created final modal with jQuery

--- js-folder/index.js
+++ js-folder/index.js
@@ -1,54 +1,31 @@
-$(".toggle").click(function(){
-  $(this).find(".toggleContainer").toggleClass("active");
-  $(this).siblings(".toggle").find(".toggleContainer").removeClass("active");
+// $(".toggle").click(function(){
+//   $(this).find(".toggleContainer").toggleClass("active");
+//   $(this).siblings(".toggle").find(".toggleContainer").removeClass("active");
+// });
+
+
+$('.readMore-value').on ('click', function(){
+  console.log("readMore was pressed")
+    $(".valueCommunity").hide();
+    $(".studios").css("opacity", 0.3);
+    $(".TSWideas").css("opacity", 0.3);
+    $("#Recess").css("opacity", 0.3);
+    $("#BlogPost").css("opacity", 0.3);
+    $(".valueCommunity").css("opacity", 0.3);
+    $(".featuredProjects").css("opacity", 0.3);
+    $("#modal").show()
 });
-
-
-
-$('.readMore value').on ('click', function(){
-    $(".modal").show("slow");
+$('.close').on ('click', function(){
+  console.log("readMore was pressed")
+    $("#modal").hide();
+    $(".studios").css("opacity", 1);
+    $(".TSWideas").css("opacity", 1);
+    $("#Recess").css("opacity", 1);
+    $("#BlogPost").css("opacity", 1);
+    $(".valueCommunity").css("opacity", 1);
+    $(".featuredProjects").css("opacity", 1);
+    $(".valueCommunity").show();
 });
-
-
-$(".readMore").dialog({
-    modal: true;
-});
-
-var modal = $(".readMore").dialog(".modal")
-$(".readMore").dialog(".modal", true);
-
-// Getter
-var position = $( ".selector" ).dialog(".modal", "position");
- 
-// Setter
-$( ".selector" ).dialog( "option", "position", { my: "left top", at: "left bottom", of: button } );
-
-
-function myFunction() { 
-  document.getElementById(".modal").showModal(); 
-} 
-
-
-
-      // example from class activities //
-      // Script to open and close sidebar
-function w3_open() {
-    document.getElementById("mySidebar").style.display = "block";
-    document.getElementById("myOverlay").style.display = "block";
-  }
-  
-  function w3_close() {
-    document.getElementById("mySidebar").style.display = "none";
-    document.getElementById("myOverlay").style.display = "none";
-  }
-  
-  // Modal Image Gallery
-  function onClick(element) {
-    document.getElementById("img01").src = element.src;
-    document.getElementById("modal01").style.display = "block";
-    var captionText = document.getElementById("caption");
-    captionText.innerHTML = element.alt;
-  }
 
 
   
